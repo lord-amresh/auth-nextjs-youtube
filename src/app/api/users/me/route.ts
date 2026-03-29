e78a95210f42a45c86a4d812bdf7261065fd8085
lord-amresh <amresh2006@gmail.com>
2026-03-29 23:47:25 +0545

    continued....

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -11,6 +11,7 @@ export async function GET(request: NextRequest) {
     try {
         const userId = await getDataFromToken(request);
         const user = await User.findOne({ _id: userId })
+            select("-password");
     } catch (error:any) {
         return NextResponse.json({ error: error.message }, { status: 400 });
         
