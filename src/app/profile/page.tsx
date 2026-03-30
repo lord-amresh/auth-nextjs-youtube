c5416a6ef8df356f81754c988344ad63d7f3df69
lord-amresh <amresh2006@gmail.com>
2026-03-30 08:35:30 +0545

    Added middleware and me route

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import axios from "axios";
-import link from "next/link";
+import Link from "next/link";
+import React, { useState } from "react";
 import {toast} from "react-hot-toast";
 import { useRouter } from "next/navigation";
 
 
 export default function ProfilePage () {
     const router = useRouter();
+    const [data, setData] = useState("nothing");
     const logout = async () => {
         try {
             await axios.get("/api/users/logout");
@@ -16,15 +18,27 @@ export default function ProfilePage () {
                 console.log(error.message);
                 toast.error( error.message);
         }
-
     }
+
+        const getUserDetails = async () => {
+            const res = await axios.get("/api/users/me");
+            console.log(res.data);
+            setData(res.data.data._id);
+        }
+
         return(
             <div className="flex flex-col items-center justify-center min-h-screen py-2">
                 <h1>Profile</h1>
                 <hr/>
                 <p>Welcome to your profile page!</p>
+                <h2 className = "p-1 rounded bg-green-500">{data === 'nothing'? "Nothing" : <Link href = {`/profile/${data}`} >{data}
+                </Link>}</h2>
                 <hr/>
                 <button onClick={logout} className="bg-red-500 text-white px-4 py-2 rounded mt-4">Logout</button>
+
+                <button 
+                onClick={getUserDetails} 
+                className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Get User Details</button>
             </div>
         ) 
 }
