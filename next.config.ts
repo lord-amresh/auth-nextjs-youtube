b363ee99287246e18873a29632bfd286de13b46c
lord-amresh <amresh2006@gmail.com>
2026-03-31 13:08:33 +0545

    few changes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  staticPageGenerationTimeout: 120,
+  experimental: {
+  }
   /* config options here */
-  reactCompiler: true,
 };
 
 export default nextConfig;
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic";
 import axios from "axios";
 import Link from "next/link";
 import React, { useState } from "react";
